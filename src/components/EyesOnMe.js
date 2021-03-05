7b7285fb919651799d4fc166fcce6bae02c3e896
ravyar aram <raviar.aramf19@komar.edu.iq>
2021-03-05 15:39:26 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 class EyesOnMe extends React.Component {
 
+  onFocus = () => console.log('Good!')
+
+  onBlur = () => console.log('Hey! Eyes on me!')
 
   render() {
     return (
